dd9f3ee10b8f4b5f072271e66a20c4018a77b86e
fusho-takahashi <zenryaku@fusho-takahashi.me>
2023-05-06 14:43:02 +0900

    form のtitle とdescription を追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { environment } from '../environments/environment';
+
+type Form = {
+  title: string;
+  description: string;
+};
 
 @Component({
   selector: 'app-root',
@@ -9,6 +17,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'simple-form-spa';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+
+  title = signal<string>('simple-form-spa');
+  form = signal<Form>({ title: '', description: '' });
+  formId = signal<string>('cf39ec32-d08b-0629-0ece-8d9b1dc97fcf');
+  url = computed(() => `${environment.api}/form/${this.formId()}`);
+
+  async ngOnInit() {
+    const res = await firstValueFrom(this.http.get<Form>(this.url()));
+    this.form.set(res);
+  }
 }
